4b334acebe0849f16342f4acb0d71ecd776d62b6
Amad Ul HASSAN <ahassan@sii.cz>
2025-06-19 14:59:55 +0200

    initial commit

---
+++ src/main/java/com/amdev/AttendenceServer/AttendenceServerApplication.java
@@ -0,0 +1,13 @@
+package com.amdev.AttendenceServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendenceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendenceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amdev/AttendenceServer/AttendenceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdev.AttendenceServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendenceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
