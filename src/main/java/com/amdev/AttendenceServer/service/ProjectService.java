211af4129e8c585fcc808e51e7258964c5f95da1
Amad Ul Hassan <ah@indigilabs.com>
2025-06-21 20:55:54 +0200

    list all projects api

--- src/main/java/com/amdev/AttendenceServer/service/ProjectService.java
+++ src/main/java/com/amdev/AttendenceServer/service/ProjectService.java
@@ -4,7 +4,13 @@ import com.amdev.AttendenceServer.dto.ProjectDTO;
 import com.amdev.AttendenceServer.entities.Project;
 import com.amdev.AttendenceServer.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProjectService {
@@ -18,5 +24,11 @@ public class ProjectService {
         project.setStartDate(dto.getStartDate());
 
         return projectRepository.save(project).getDto();
+    }
+
+
+
+    public List<ProjectDTO> getAllProjects(){
+        return projectRepository.findAll().stream().map(Project::getDto).collect(Collectors.toList());
     }
 }
